59ed37f4436b5a9480bd118af37de90ae22bfbda
Emanuel Rosal <emanuel.rosa@naranja.com>
2020-07-22 23:01:32 -0300

    feat: add cloudflare

--- README.md
+++ README.md
@@ -1,20 +1,9 @@
-## Base Template for Serverless Plugin
+## Serverless Firewall Plugin
 [![serverless](http://public.serverless.com/badges/v3.svg)](http://www.serverless.com)
-[![npm version](https://badge.fury.io/js/template-base-serverless-plugin.svg)](https://badge.fury.io/js/template-base-serverless-plugin)
-[![npm downloads](https://img.shields.io/npm/dt/template-base-serverless-plugin.svg?style=flat)](https://www.npmjs.com/package/template-base-serverless-plugin)
+[![npm version](https://badge.fury.io/js/template-base-serverless-plugin.svg)](https://badge.fury.io/js/serverless-firewall-plugin)
+[![npm downloads](https://img.shields.io/npm/dt/serverless-firewall-plugin.svg?style=flat)](https://www.npmjs.com/package/serverless-firewall-plugin)
 
 ### Installation
 ```bash
-git clone https://github.com/w4rlock/template-base-serverless-plugin.git
-
-Or
-
-Click green button "Use this template"
+npm i -E serverless-waf-allow-cloudflare-plugin
 ```
-
-### Usage
-To do Use generator to render:
-- README.md
-- index.js
-- serverless.yml
-
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const _ = require('lodash');
 const BaseServerlessPlugin = require('base-serverless-plugin');
+const utils = require('./src/utils');
 
-const LOG_PREFFIX = '[ServerlessPlugin] -';
-const USR_CONF = 'pluginConfig';
+const LOG_PREFFIX = '[ServerlessFirewall] -';
+const USR_CONF = 'cdnStack';
 
 class ServerlessPlugin extends BaseServerlessPlugin {
   /**
@@ -12,11 +14,13 @@ class ServerlessPlugin extends BaseServerlessPlugin {
    */
   constructor(serverless, options) {
     super(serverless, options, LOG_PREFFIX, USR_CONF);
+    Object.assign(this, utils);
+
+    this.pluginPath = __dirname;
+    this.onceInit = _.once(() => this.initialize());
 
     this.hooks = {
-      'after:deploy:deploy': this.dispatchAction.bind(this, this.deploy),
-      'after:info:info': this.dispatchAction.bind(this, this.info),
-      'after:remove:remove': this.dispatchAction.bind(this, this.remove),
+      'package:initialize': this.dispatchAction.bind(this, this.injectTemplate),
     };
   }
 
@@ -24,6 +28,7 @@ class ServerlessPlugin extends BaseServerlessPlugin {
    * Action Wrapper check plugin condition before perform action
    *
    * @param {function} funAction serverless plugin action
+   *
    */
   async dispatchAction(funAction, varResolver = undefined) {
     if (this.isPluginDisabled()) {
@@ -31,42 +36,46 @@ class ServerlessPlugin extends BaseServerlessPlugin {
       return '';
     }
 
-    this.loadConfig();
+    await this.onceInit();
     return funAction.call(this, varResolver);
   }
 
   /**
-   * Load user config
+   * Initialize and call another hook plugin
    *
+   * @returns {promise} another hook plugin promise if need
    */
-  loadConfig() {
+  async initialize() {
     this.cfg = {};
-    this.cfg.prop = this.getConf('prop', 'default_value');
-    this.cfg.requiredProp = this.getConf('prop');
+    this.cfg.scope = 'CLOUDFRONT';
+    // CLOUD FLARE IPS
+    this.cfg.whiteListIpsBlock = [
+      '173.245.48.0/20',
+      '103.21.244.0/22',
+      '103.22.200.0/22',
+      '103.31.4.0/22',
+      '141.101.64.0/18',
+      '108.162.192.0/18',
+      '190.93.240.0/20',
+      '188.114.96.0/20',
+      '197.234.240.0/22',
+      '198.41.128.0/17',
+      '162.158.0.0/15',
+      '104.16.0.0/12',
+      '172.64.0.0/13',
+      '131.0.72.0/22',
+    ];
+
+    return Promise.resolve();
   }
 
   /**
-   * Deploy
+   * Add yml templates rendered on demand
+   * to CloudFormation template
    *
    */
-  async deploy() {
-    this.log('Deploy...');
-  }
-
-  /**
-   * Info
-   *
-   */
-  async info() {
-    this.log('Info...');
-  }
-
-  /**
-   * Remove
-   *
-   */
-  async remove() {
-    this.log('Removing...');
+  async injectTemplate() {
+    this.mergeCFormationResources(this.render('./resources/waf.hbs', this.cfg));
   }
 }
 
